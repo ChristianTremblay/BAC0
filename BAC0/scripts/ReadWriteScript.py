f73fb0ccc7b578d01975a2c7df4352a33492c4b5
Christian Tremblay, ing <christian.tremblay@servisys.com>
2015-06-15 17:53:53 -0400

    Renamed exampleScript which is in fact a special script (Read Write, based on BasicScript)
    Signed-off-by: Christian Tremblay, ing. <christian.tremblay@servisys.com>

---
+++ BAC0/scripts/ReadWriteScript.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+
+"""
+Built around a simple BIPSimpleApplication this class allows to create read and write
+requests and store read responses in a variables
+
+For 'read' commands it will create ReadPropertyRequest PDUs, then lines up the
+coorresponding ReadPropertyACK and return the value. 
+
+For 'write' commands it will create WritePropertyRequst PDUs and prints out a simple acknowledgement.
+"""
+
+from bacpypes.debugging import bacpypes_debugging, ModuleLogger
+
+from BAC0.scripts.BasicScript import BasicScript
+from BAC0.core.io import Read, Write
+
+# some debugging
+_debug = 0
+_log = ModuleLogger(globals())
+
+@bacpypes_debugging
+class ReadWriteScript(BasicScript,Read.ReadProperty,Write.WriteProperty):
+    """ 
+    This class build a running bacnet application and will accept read ans write requests
+    
+    """
+    def __init__(self, localIPAddr = '127.0.0.1', localObjName = 'name', Boid = '2015',maxAPDULengthAccepted = '1024',segmentationSupported = 'segmentedBoth', vendorID = '15' ):
+        """
+        Initialization requires information on the local device
+        Default values are localObjName = 'name', Boid = '2015',maxAPDULengthAccepted = '1024',segmentationSupported = 'segmentedBoth', vendorID = '15' )
+        Local IP address must be given in a string.
+        Normally, the address must be in the same subnet than the bacnet network (if no BBMD or Foreign device is used)        
+        """     
+        if _debug: _log.debug("Configurating app")
+        BasicScript.__init__(self, localIPAddr = localIPAddr, localObjName = localObjName, Boid = Boid,maxAPDULengthAccepted = maxAPDULengthAccepted,segmentationSupported = segmentationSupported, vendorID = vendorID )
+        
+        # Force and gloab whois to find all devices on the network
+        self.whois()
+   
+    #def discover(self,addr):     
+    #    bf.discoverPoints(self,addr)
+            
+            
+#
+#   __main__
+#
+if __name__ == '__main__':
+    bacnet = ReadWriteScript(localIPAddr = '192.168.210.63')
+
+
+
+
