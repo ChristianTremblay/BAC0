0125c107f6937c707a983b8ccb9bf4a47a267fef
Christian Tremblay <christian.tremblay@servisys.com>
2015-11-02 13:19:18 -0500

    Trying to modify Stop It test so Travis-CI will handle it correctly
    Signed-off-by: Christian Tremblay <christian.tremblay@servisys.com>

--- tests/TestReadWriteScript.py
+++ tests/TestReadWriteScript.py
@@ -21,9 +21,13 @@ class TestReadWriteScript(unittest.TestCase):
         Setup Test, should start script
         """
         self.bacnet = BAC0.ReadWriteScript()
+        
+    def test_bacnet_started(self):
+        assert self.bacnet._started == True
 
     def test_bacnet_network_stop(self):
         """
         Stop it
         """
         self.bacnet.stopApp()
+        assert self.bacnet._started == False
