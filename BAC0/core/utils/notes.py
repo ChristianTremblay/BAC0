82e8ee98c5aa9737cb64c8a818c0cb79caeab95a
ctremblay <ctremblay@linux.bachand-tremblay.com>
2018-01-21 00:14:45 -0500

    Working on note_and_log decorator. This will allow taking notes easily accessible inside the app and everything will be on file

--- BAC0/core/utils/notes.py
+++ BAC0/core/utils/notes.py
@@ -7,6 +7,7 @@ Notes object used in device and network
 #--- standard Python modules ---
 from collections import namedtuple
 from datetime import datetime
+import logging
 
 #--- 3rd party modules ---
 import pandas as pd
@@ -29,4 +30,30 @@ class Notes():
         notes_table = pd.Series(self._notes.notes, index=self._notes.timestamp)
         return notes_table  
 
-        
+def note_and_log(cls):
+    cls._notes = namedtuple('_notes',['timestamp', 'notes'])
+    cls._notes.timestamp = []
+    cls._notes.notes = []
+    logname = 'BAC0'
+    cls._log = logging.getLogger(logname)
+    ch = logging.StreamHandler()
+    ch.setLevel = logging.ERROR
+    fh = logging.FileHandler('BAC0.log')
+    fh.setLevel = logging.DEBUG
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    fh.setFormatter(formatter)
+    cls._log.addHandler(ch)
+    cls._log.addHandler(fh)
+    
+    def add_note(self, note='', level=logging.INFO):
+        cls._notes.timestamp.append(datetime.now())
+        cls._notes.notes.append(note)
+        cls._log.log(level, note)
+    
+    def get_notes(self):
+        return pd.Series(self._notes.notes, index=self._notes.timestamp)
+
+    cls.add_note = add_note
+    cls.get_notes = get_notes
+    return cls
