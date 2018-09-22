f686d30f03fc9d2661fd72109c85f90454f3aaa0
Christian Tremblay, ing <christian.tremblay@servisys.com>
2018-09-21 20:58:37 -0400

    Cleanup of html files with bootstrap

--- BAC0/web/templates.py
+++ BAC0/web/templates.py
@@ -8,42 +8,34 @@ Created on Mon Nov 13 21:37:02 2017
 
 def create_sidebar(dash_class="", devices_class="", trends_class=""):
     sb = """
-    	<div class="wrapper">
+    <div class="wrapper">
     <div class="sidebar" data-background-color="white" data-active-color="danger">
-
     <!--
-		Tip 1: you can change the color of the sidebar's background using: data-background-color="white | black"
-		Tip 2: you can change the color of the active button using the data-active-color="primary | info | success | warning | danger"
-	-->
-
-    	<div class="sidebar-wrapper">
-            <div class="logo">
-                <a href="https://github.com/ChristianTremblay/BAC0" class="simple-text">
-                    BAC0
-                </a>
-            </div>
-
-            <ul class="nav">
-                <li %s>
-                    <a href="/">
-                        <i class="ti-panel"></i>
-                        <p>Dashboard</p>
-                    </a>
-                </li>
-                <li %s>
-                    <a href="/trends">
-                        <i class="ti-panel"></i>
-                        <p>Trend</p>
-                    </a>
-                </li>
-                <li %s>
-                    <a href="/dash_devices">
-                        <i class="ti-view-list-alt"></i>
-                        <p>Devices</p>
-                    </a>
-                </li>
-            </ul>
-    	</div>
+    Tip 1: you can change the color of the sidebar's background using: data-background-color="white | black"
+    Tip 2: you can change the color of the active button using the data-active-color="primary | info | success | warning | danger"
+    -->
+    <div class="sidebar-wrapper">
+      <div class="logo">
+      <a href="https://github.com/ChristianTremblay/BAC0" class="simple-text">BAC0</a>
+      </div>
+      <ul class="nav">
+        <li %s><a href="/">
+               <i class="ti-panel"></i>
+               <p>Dashboard</p>
+               </a>
+        </li>
+        <li %s><a href="/trends">
+               <i class="ti-panel"></i>
+               <p>Trend</p>
+               </a>
+        </li>
+        <li %s><a href="/dash_devices">
+               <i class="ti-view-list-alt"></i>
+               <p>Devices</p>
+               </a>
+        </li>
+      </ul>
+      </div>
     </div>
     """ % (dash_class, trends_class, devices_class)
     return sb
@@ -73,27 +65,26 @@ def create_card(icon='ti-server', title='title',
     card = """
         <div class="col-lg-3 col-sm-6">
         <div class="card">
-            <div class="content">
-                <div class="row">
-                    <div class="col-xs-5">
-                        <div class="icon-big icon-success text-center">
-                
-            <i class="%s"></i>
-                        </div>
-                    </div>
-                    <div class="col-xs-7">
-                        <div class="numbers">
-                            <p>%s</p>
-                            <div id="%s">%s</div>
-                        </div>
-                    </div>
+          <div class="content">
+            <div class="row">
+              <div class="col-xs-5">
+                <div class="icon-big icon-success text-center">
+                  <i class="%s"></i>
                 </div>
-                <div class="footer">
-                    <hr />
-                    <div class="stats">
-                        <i class="%s"></i> <div id="%s">%s</div>
-                    </div>
+              </div>
+                <div class="col-xs-7">
+                  <div class="numbers">
+                    <p>%s</p>
+                    <div id="%s">%s</div>
+                  </div>
                 </div>
+              </div>
+            <div class="footer">
+            <hr />
+            <div class="stats">
+            <i class="%s"></i> <div id="%s">%s</div>
+            </div>
+            </div>
             </div>
         </div>
         </div>
