bc9cfbe0dad92ae541dcbe3e4da32bc51f6fe438
Christian Tremblay <christian.tremblay@servisys.com>
2018-10-01 10:05:57 -0400

    Continuing tests
    Signed-off-by: Christian Tremblay <christian.tremblay@servisys.com>

--- BAC0/core/devices/create_objects.py
+++ BAC0/core/devices/create_objects.py
@@ -1,9 +1,9 @@
-from bacpypes.object import MultiStateValueObject
+from bacpypes.object import MultiStateValueObject,AnalogValueObject, BinaryValueObject, Property, register_object_type
 from bacpypes.primitivedata import CharacterString
 from bacpypes.constructeddata import ArrayOf
+from bacpypes.primitivedata import Real, Boolean
+from bacpypes.basetypes import EngineeringUnits
 
-from bacpypes.primitivedata import Real
-from bacpypes.object import AnalogValueObject, Property, register_object_type
 
 def create_MV(oid=1, pv=0, name='MV', states=['red','green','blue']):
     msvo = MultiStateValueObject(
@@ -15,10 +15,21 @@ def create_MV(oid=1, pv=0, name='MV', states=['red','green','blue']):
            )
     return msvo
 
-def create_AV(oid=1, pv=0, name='AV', units=''):
+def create_AV(oid=1, pv=0, name='AV', units=None):
     avo = AnalogValueObject(
            objectIdentifier=('analogValue', oid),
            objectName=name,
            presentValue=pv,
+           units=units
            )
     return avo
+
+def create_BV(oid=1, pv=0, name='BV', activeText='On', inactiveText='Off'):
+    bvo = BinaryValueObject(
+           objectIdentifier=('binaryValue', oid),
+           objectName=name,
+           presentValue=pv,
+           activeText=activeText,
+           inactiveText=inactiveText
+           )
+    return bvo
--- tests/test_WithFakeDevice.py
+++ tests/test_WithFakeDevice.py
@@ -7,32 +7,22 @@ Test Bacnet communication with another device
 
 import pytest
 import BAC0
-import time
 
-from BAC0.core.devices.create_objects import create_AV, create_MV
+from BAC0.core.devices.create_objects import create_AV, create_MV, create_BV
 
-# We'll need a device
-
-
-@pytest.fixture
+@pytest.fixture(scope='session')
 def network_and_devices():
     bacnet = BAC0.connect()
-    while not bacnet._started:
-        time.sleep(0.1)
-    # print('{}'.format(bacnet))
 
     device_app = BAC0.lite(port=47809)
-    while not device_app._started:
-        time.sleep(0.1)
-    # print('{}'.format(device_app))
-
     mv = create_MV(oid=1, name='mv', pv=1)
     av = create_AV(oid=1, name='av', pv=99.9)
+    bv = create_BV(oid=1, name='bv', pv=1)
 
-    # Adding two object to the fake device
+    # Adding objects to the fake device
     device_app.this_application.add_object(mv)
     device_app.this_application.add_object(av)
-    #print('Device App : {}'.format(device_app))
+    device_app.this_application.add_object(bv)
 
     ip = device_app.localIPAddr.addrTuple[0]
     boid = device_app.Boid
@@ -40,20 +30,19 @@ def network_and_devices():
     # Connect to test device using main network
     test_device = BAC0.device('{}:47809'.format(ip), boid, bacnet)
 
-    return (bacnet, device_app, test_device)
-
-
-def test_ReadAV(network_and_devices):
-    bacnet, device_app, test_device = network_and_devices
-    print('{}'.format(test_device.points))
-    print('{}'.format(test_device['av'].value))
-    assert (test_device['av'] - 99.90) < 0.01
+    yield (bacnet, device_app, test_device)
+    # Close when done
     bacnet.disconnect()
     device_app.disconnect()
 
+def test_ReadAV(network_and_devices):
+    bacnet, device_app, test_device = network_and_devices
+    assert (test_device['av'] - 99.90) < 0.01
 
 def test_ReadMV(network_and_devices):
     bacnet, device_app, test_device = network_and_devices
     assert test_device['mv'].value == 1
-    bacnet.disconnect()
-    device_app.disconnect()
+    
+def test_ReadBV(network_and_devices):
+    bacnet, device_app, test_device = network_and_devices
+    assert test_device['bv'].value == 'active'
