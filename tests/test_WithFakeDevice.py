511b48beafe8ac4c68d73d9653d359a1c6bb063d
Christian Tremblay, ing <christian.tremblay@servisys.com>
2018-09-30 21:40:55 -0400

    First test is working. Able to validate one AV and one MV... I'll be able to test every functions now@

--- tests/test_WithFakeDevice.py
+++ tests/test_WithFakeDevice.py
@@ -12,54 +12,48 @@ import time
 from BAC0.core.devices.create_objects import create_AV, create_MV
 
 # We'll need a device
-global bacnet
-global device_app
-global test_device
-
-bacnet = BAC0.connect()
-while not bacnet._started:
-    time.sleep(0.1)
-print('{}'.format(bacnet))
-
-device_app = BAC0.lite(port=47809)
-while not device_app._started:
-    time.sleep(0.1)
-print('{}'.format(device_app))
-
-mv = create_MV(oid=1, name='mv', pv=1)
-av = create_AV(oid=1, name='av', pv=99.9)
-
-# Adding two object to the fake device
-device_app.this_application.add_object(mv)
-device_app.this_application.add_object(av)
-print('Device App : {}'.format(device_app))
-
-test_device = None
 
 
-def test_DeviceCreation():
-    global bacnet
-    global test_device
-    global device_app
+@pytest.fixture
+def network_and_devices():
+    bacnet = BAC0.connect()
+    while not bacnet._started:
+        time.sleep(0.1)
+    # print('{}'.format(bacnet))
+
+    device_app = BAC0.lite(port=47809)
+    while not device_app._started:
+        time.sleep(0.1)
+    # print('{}'.format(device_app))
+
+    mv = create_MV(oid=1, name='mv', pv=1)
+    av = create_AV(oid=1, name='av', pv=99.9)
+
+    # Adding two object to the fake device
+    device_app.this_application.add_object(mv)
+    device_app.this_application.add_object(av)
+    #print('Device App : {}'.format(device_app))
+
     ip = device_app.localIPAddr.addrTuple[0]
     boid = device_app.Boid
 
     # Connect to test device using main network
     test_device = BAC0.device('{}:47809'.format(ip), boid, bacnet)
 
+    return (bacnet, device_app, test_device)
 
-def test_ReadAV():
-    global test_device
+
+def test_ReadAV(network_and_devices):
+    bacnet, device_app, test_device = network_and_devices
     print('{}'.format(test_device.points))
     print('{}'.format(test_device['av'].value))
     assert (test_device['av'] - 99.90) < 0.01
+    bacnet.disconnect()
+    device_app.disconnect()
 
 
-def test_ReadMV():
-    global test_device
+def test_ReadMV(network_and_devices):
+    bacnet, device_app, test_device = network_and_devices
     assert test_device['mv'].value == 1
-
-
-def test_disconnect():
-    test_device.properties.network.disconnect()
+    bacnet.disconnect()
     device_app.disconnect()
