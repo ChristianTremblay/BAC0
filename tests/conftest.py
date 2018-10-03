868070a603a4aff2409f3b1f4638ce0743684bcd
Christian Tremblay <christian.tremblay@servisys.com>
2018-10-03 14:02:51 -0400

    New tests for BAC0 including segmentation of server mode
    Signed-off-by: Christian Tremblay <christian.tremblay@servisys.com>

---
+++ tests/conftest.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# -*- coding utf-8 -*-
+
+"""
+Test Bacnet communication with another device
+"""
+
+import pytest
+import BAC0
+
+from BAC0.core.devices.create_objects import create_AV, create_MV, create_BV
+
+from collections import namedtuple
+
+@pytest.fixture(scope='session')
+def network_and_devices():
+    bacnet = BAC0.connect()
+
+    def _add_points(qty, device):
+        # Add a lot of points for tests (segmentation required)
+        qty_of_avs = qty
+        qty_of_bvs = qty
+        qty_of_mvs = qty
+        
+        mvs = []
+        avs = []
+        bvs = []
+        
+        for i in range(qty_of_avs):
+            mvs.append(create_MV(oid=i, name='mv{}'.format(i), pv=1))
+        for i in range(qty_of_mvs):    
+            avs.append(create_AV(oid=i, name='av{}'.format(i), pv=99.9))
+        for i in range(qty_of_bvs):
+            bvs.append(create_BV(oid=i, name='bv{}'.format(i), pv=1))
+
+        for mv in mvs:
+            device.this_application.add_object(mv)
+        for av in avs:
+            device.this_application.add_object(av)
+        for bv in bvs:
+            device.this_application.add_object(bv)
+            
+    
+    # We'll use 3 devices with our first instance
+    device_app = BAC0.lite(port=47809)
+    device30_app = BAC0.lite(port=47810)
+    device300_app = BAC0.lite(port=47811)
+    
+    _add_points(1,device_app)
+    _add_points(10,device30_app)
+    _add_points(100,device300_app)
+    
+   
+    ip = device_app.localIPAddr.addrTuple[0]
+    boid = device_app.Boid
+    
+    ip_30 = device30_app.localIPAddr.addrTuple[0]
+    boid_30 = device30_app.Boid
+
+    ip_300 = device300_app.localIPAddr.addrTuple[0]
+    boid_300 = device300_app.Boid
+
+    # Connect to test device using main network
+    test_device = BAC0.device('{}:47809'.format(ip), boid, bacnet, poll=120)
+    test_device_30 = BAC0.device('{}:47810'.format(ip_30), boid_30, bacnet, poll=120)
+    test_device_300 = BAC0.device('{}:47811'.format(ip_300), boid_300, bacnet, poll=120)
+    
+    params = namedtuple('devices',['bacnet','device_app','test_device','test_device_30','test_device_300'])
+    params.bacnet = bacnet
+    params.device_app = device_app
+    params.test_device = test_device
+    params.test_device_30 = test_device_30
+    params.test_device_300 = test_device_300
+
+
+    yield params
+    
+    # Close when done
+    test_device.disconnect()
+    test_device_30.disconnect()
+    test_device_300.disconnect()
+
+    device_app.disconnect()
+    device30_app.disconnect()
+    device300_app.disconnect()
+    
+    bacnet.disconnect()
+
---
+++ tests/test_BacpypesVersion.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding utf-8 -*-
+
+"""
+Test Bacpypes version
+"""
+
+import bacpypes
+
+def test_BacpypesVersion():
+    maj, minor, patch = bacpypes.__version__.split('.')
+    assert float(maj) == 0
+    assert float(minor) == 17
+    assert float(patch) == 6
