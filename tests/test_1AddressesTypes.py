cb5966262bf44d45d962c25f2ea1b4e3b8ec4cdb
Christian Tremblay, ing <christian.tremblay@servisys.com>
2018-10-13 10:29:01 -0400

    Fix to a problem I introduced in Bokeh Legend.... Created a fixture for HostIP in Address test hoping it'll fix TravisCI testing failing...even if tests work locally

--- tests/test_1AddressesTypes.py
+++ tests/test_1AddressesTypes.py
@@ -7,43 +7,41 @@ Test Bacnet communication with another device
 
 from BAC0.core.functions.GetIPAddr import HostIP
 import BAC0
+import pytest
 
-def test_WithIPProvided():
-    # Disconnect conftest ?
+@pytest.fixture(scope='module')
+def host_ip():
     hip = HostIP()
     ip, subnet = hip.ip_address_subnet.split('/')
+    return (ip,subnet)
+
+
+def test_WithIPProvided(host_ip):
+    ip, subnet = host_ip
     bacnet_with_ip = BAC0.lite(ip=ip,port=47812)
     assert bacnet_with_ip.vendorId == 842
     bacnet_with_ip.disconnect()
 
-def test_WithIPAndMask():
-    # Disconnect conftest ?
-    hip = HostIP()
-    ip, subnet = hip.ip_address_subnet.split('/')
+def test_WithIPAndMask(host_ip):
+    ip, subnet = host_ip
     bacnet_with_ip = BAC0.lite(ip=ip, mask=subnet, port=47812)
     assert bacnet_with_ip.vendorId == 842
     bacnet_with_ip.disconnect()
     
-def test_WithIPAndMaskAndPort():
-    # Disconnect conftest ?
-    hip = HostIP()
-    ip, subnet = hip.ip_address_subnet.split('/')
+def test_WithIPAndMaskAndPort(host_ip):
+    ip, subnet = host_ip
     bacnet_with_ip = BAC0.lite(ip=ip, mask=subnet, port=47812)
     assert bacnet_with_ip.vendorId == 842
     bacnet_with_ip.disconnect()
     
-def test_WithIPAndMaskInString():
-    # Disconnect conftest ?
-    hip = HostIP()
-    ip, subnet = hip.ip_address_subnet.split('/')
+def test_WithIPAndMaskInString(host_ip):
+    ip, subnet = host_ip
     bacnet_with_ip = BAC0.lite(ip='{}/{}'.format(ip,subnet),port=47812)
     assert bacnet_with_ip.vendorId == 842
     bacnet_with_ip.disconnect()
     
-def test_WithIPAndMaskAndPortInString():
-    # Disconnect conftest ?
-    hip = HostIP()
-    ip, subnet = hip.ip_address_subnet.split('/')
+def test_WithIPAndMaskAndPortInString(host_ip):
+    ip, subnet = host_ip
     bacnet_with_ip = BAC0.lite(ip='{}/{}:47812'.format(ip,subnet))
     assert bacnet_with_ip.vendorId == 842
     bacnet_with_ip.disconnect() 
