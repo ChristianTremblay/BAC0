d7565e3ef806261c7e6069812b53fe915c86cabf
Drew Massey <drew@gramlabs.ai>
2018-12-07 11:48:24 -0500

    Light cleanup on tests to avoid reptition of constants

--- tests/test_Read.py
+++ tests/test_Read.py
@@ -5,9 +5,13 @@
 Test Bacnet communication with another device
 """
 
+CHANGE_DELTA = 99.90
+TOLERANCE = 0.01
+BINARY_TEST_STATE = 'active'
+
 def test_ReadAV(network_and_devices):
     test_device = network_and_devices.test_device
-    assert (test_device['av0'] - 99.90) < 0.01
+    assert (test_device['av0'] - CHANGE_DELTA) < TOLERANCE
 
 def test_ReadMV(network_and_devices):
     test_device = network_and_devices.test_device
@@ -15,20 +19,20 @@ def test_ReadMV(network_and_devices):
     
 def test_ReadBV(network_and_devices):
     test_device = network_and_devices.test_device
-    assert test_device['bv0'].value == 'active'
+    assert test_device['bv0'].value == BINARY_TEST_STATE
 
 def test_ReadAI(network_and_devices):
     test_device = network_and_devices.test_device
-    assert (test_device['ai0'] - 99.90) < 0.01
+    assert (test_device['ai0'] - CHANGE_DELTA) < TOLERANCE
     
 def test_ReadAO(network_and_devices):
     test_device = network_and_devices.test_device
-    assert (test_device['ao0'] - 99.90) < 0.01
+    assert (test_device['ao0'] - CHANGE_DELTA) < TOLERANCE
     
 def test_ReadBI(network_and_devices):
     test_device = network_and_devices.test_device
-    assert test_device['bi0'].value == 'active'
+    assert test_device['bi0'].value == BINARY_TEST_STATE
     
 def test_ReadBO(network_and_devices):
     test_device = network_and_devices.test_device
-    assert test_device['bo0'].value == 'active'
+    assert test_device['bo0'].value == BINARY_TEST_STATE
