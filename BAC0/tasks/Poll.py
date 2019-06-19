e1dcb3d920e0621b7d2884db3b6c5f97e53f65d6
Christian Tremblay <christian.tremblay@servisys.com>
2019-06-18 23:43:36 -0400

    Bumping version

--- BAC0/tasks/Poll.py
+++ BAC0/tasks/Poll.py
@@ -90,5 +90,7 @@ class DevicePoll(Task):
             # So kill the task
             self.stop()
         except ValueError:
-            self.device._log.error('Something is wrong with polling...stopping. Try setting off segmentation')
+            self.device._log.error(
+                "Something is wrong with polling...stopping. Try setting off segmentation"
+            )
             self.stop()
