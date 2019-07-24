8e01fbd0f0621d5bfa7373eede7c85b0ac45b3d8
Christian Tremblay <christian.tremblay@servisys.com>
2019-07-24 16:41:17 -0400

    New implementation for proprietary objects

--- BAC0/core/proprietary_objects/jci.py
+++ BAC0/core/proprietary_objects/jci.py
@@ -3,32 +3,28 @@
 """
 Johnson Controls Proprietary Objects for FX/FEC Line
 """
-from bacpypes.primitivedata import Boolean
-from bacpypes.object import BinaryValueObject, Property, register_object_type
+from bacpypes.primitivedata import Boolean, CharacterString
+from bacpypes.object import (
+    DeviceObject,
+    BinaryValueObject,
+    Property,
+    register_object_type,
+)
 
 #
-#   Vendor TECSupOnline Value Object Type
+#   Proprietary Objects and their attributes
 #
 
-
-class TECSupOnline(BinaryValueObject):
-    """
-    This class will provide to a BAC0 device referring to a Johnson Controls TEC3000
-    thermostats the Supervisor Online variable. This will allow testing of remote
-    occupancy schedules.
-    """
-
-    objectType = "device"
-    vendor_id = 5
-
-    properties = [Property(3653, Boolean, mutable=True)]
-
-    def __init__(self, **kwargs):
-        BinaryValueObject.__init__(self, **kwargs)
-
-
-def register(cls, vendor_id=5):
-    register_object_type(cls, vendor_id=vendor_id)
+JCI_DeviceObject = {
+    "name": "JCI_DeviceObject",
+    "vendor_id": 5,
+    "objectType": "device",
+    "bacpypes_type": DeviceObject,
+    "properties": {
+        "SupervisorOnline": {"obj_id": 3653, "primitive": Boolean, "mutable": True},
+        "Model": {"obj_id": 1320, "primitive": CharacterString, "mutable": False},
+    },
+}
 
 
 def tec_short_point_list():
