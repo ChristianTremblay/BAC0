ad2f0636742c9f9d6c5553149eab3fe78a44cbda
Christian Tremblay, ing <christian.tremblay@servisys.com>
2020-02-04 15:09:58 -0500

    Modifying bacpypes version tests

--- tests/test_0BacpypesVersion.py
+++ tests/test_0BacpypesVersion.py
@@ -9,7 +9,13 @@ import bacpypes
 
 
 def test_BacpypesVersion():
+    """
+    The version format gives 3 parts and I would want for example
+    17.5 being smaller than 17.100
+    I'll trick the numbers in a way that will cover "most cases"
+    Counting patch divided by 1000 should be enough.
+    For comparison, 17.5 becomes 17.005
+    """
     maj, minor, patch = bacpypes.__version__.split(".")
-    assert float(maj) == 0
-    assert float(minor) == 17
-    assert float(patch) >= 5
+    version = float(maj) * 1000000 + float(minor) + float(patch)/1000
+    assert version >= 17.005
