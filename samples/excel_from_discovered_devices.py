d16ee556298072ce3cb106e6b272c7ca8d98fa36
Christian Tremblay, ing <christian.tremblay@servisys.com>
2021-10-17 20:47:12 -0400

    This was a request for a user... make sense to put in the samples

--- samples/excel_from_discovered_devices.py
+++ samples/excel_from_discovered_devices.py
@@ -1,5 +1,5 @@
 import BAC0
-import time 
+import time
 import pandas as pd
 from BAC0.core.proprietary_objects.jci import tec_short_point_list
 
@@ -8,7 +8,8 @@ This sample creates an Excel file containing one sheet per controller found on t
 Each sheet contyains all the points known by BAC0. Some proprietary point could not display here.
 """
 
-EXCEL_FILE_NAME = 'all_controllers_and_points.xlsx'
+EXCEL_FILE_NAME = "all_controllers_and_points.xlsx"
+
 
 def create_data(discovered_devices, network):
     devices = {}
@@ -16,26 +17,32 @@ def create_data(discovered_devices, network):
     for each in discovered_devices:
         name, vendor, address, device_id = each
 
-        #try excep eventually as we may have some issues with werid devices
-        if 'TEC3000' in name:
+        # try excep eventually as we may have some issues with werid devices
+        if "TEC3000" in name:
             custom_obj_list = tec_short_point_list()
         else:
             custom_obj_list = None
-        devices[name] = BAC0.device(address, device_id, network, poll=0, object_list = custom_obj_list)
+        devices[name] = BAC0.device(
+            address, device_id, network, poll=0, object_list=custom_obj_list
+        )
 
-        # While we are here, make a dataframe with device 
+        # While we are here, make a dataframe with device
         dataframes[name] = make_dataframe(devices[name])
     return (devices, dataframes)
-        
+
+
 def make_dataframe(dev):
     lst = {}
     for each in dev.points:
-        lst[each.properties.name] = {"value": each.lastValue, 
-                                    "units or states": each.properties.units_state,
-                                    "description": each.properties.description, 
-                                    "object": "{}:{}".format(each.properties.type, each.properties.address)}
-    df = pd.DataFrame.from_dict(lst, orient='index')
+        lst[each.properties.name] = {
+            "value": each.lastValue,
+            "units or states": each.properties.units_state,
+            "description": each.properties.description,
+            "object": "{}:{}".format(each.properties.type, each.properties.address),
+        }
+    df = pd.DataFrame.from_dict(lst, orient="index")
     return df
+
 
 def make_excel(dfs):
     with pd.ExcelWriter(EXCEL_FILE_NAME) as writer:
@@ -46,10 +53,11 @@ def make_excel(dfs):
 def main():
     bacnet = BAC0.lite()
     bacnet.discover()
-    print('Creating DATA')
+    print("Creating DATA")
     devices, dataframes = create_data(bacnet.devices, network=bacnet)
-    print('Creating and Excel File')
+    print("Creating and Excel File")
     make_excel(dataframes)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
