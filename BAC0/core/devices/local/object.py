783bfc2cab87cc932a28189427f12d19542030db
Christian Tremblay, ing <christian.tremblay@servisys.com>
2021-10-31 16:34:58 -0400

    Added support to add local objects to Foerign and BBMD BAC0 app

--- BAC0/core/devices/local/object.py
+++ BAC0/core/devices/local/object.py
@@ -2,7 +2,7 @@ from .decorator import bacnet_properties, make_commandable, create
 
 from ...utils.notes import note_and_log
 from ....scripts.Base import Base
-from ...app.ScriptApplication import BAC0Application
+from ...app.ScriptApplication import BAC0Application, BAC0BBMDDeviceApplication, BAC0ForeignDeviceApplication
 
 from bacpypes.object import (
     AnalogInputObject,
@@ -182,7 +182,7 @@ class ObjectFactory(object):
     def add_objects_to_application(self, app):
         if isinstance(app, Base):
             app = app.this_application
-        if not isinstance(app, BAC0Application):
+        if not (isinstance(app, BAC0Application) or isinstance(app, BAC0ForeignDeviceApplication) or isinstance(app, BAC0BBMDDeviceApplication)):
             raise TypeError("Provide BAC0Application object or BAC0 Base instance")
         for k, v in self.objects.items():
             try:
