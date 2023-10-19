e8fba5c43681ac2c4b727021a5e6cc4dbd747265
Christian Tremblay, ing. <christian.tremblay@servisys.com>
2023-10-18 22:16:14 -0400

    Adding more async function...untested

--- BAC0/core/functions/EventEnrollment.py
+++ BAC0/core/functions/EventEnrollment.py
@@ -39,7 +39,7 @@ class EventEnrollment:
             if not isinstance(apdu, SimpleAckPDU):  # expect an ACK
                 self._log.warning("Not an ack, see debug for more infos.")
                 self._log.debug(
-                    "Not an ack. | APDU : {} / {}".format((apdu, type(apdu)))
+                    f"Not an ack. | APDU : {apdu} / {type(apdu)}"
                 )
                 return
         if iocb.ioError:  # unsuccessful: error/reject/abort
