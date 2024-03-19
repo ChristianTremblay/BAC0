551f72963df2dd79c4548bb6f461c020700c3075
Christian Tremblay, ing. <christian.tremblay@servisys.com>
2024-03-19 17:20:41 -0400

    Fixing discover and now support localbroadcast, etc to not send global for nothing

--- BAC0/core/functions/Discover.py
+++ BAC0/core/functions/Discover.py
@@ -2,18 +2,21 @@ import asyncio
 import typing as t
 
 from bacpypes3.app import Application
-from bacpypes3.pdu import Address
+from bacpypes3.pdu import Address, LocalBroadcast
 from bacpypes3.primitivedata import ObjectIdentifier
 
 from BAC0.core.app.asyncApp import BAC0Application
 
 from ...core.utils.notes import note_and_log
+
 try:
     from rich.table import Table
     from rich.console import Console
+
     RICH = True
 except ImportError:
     RICH = False
+
 
 @note_and_log
 class Discover:
@@ -51,6 +54,7 @@ class Discover:
         networks: t.Union[str, t.List[int], int] = "known",
         limits: t.Tuple[int, int] = (0, 4194303),
         global_broadcast: bool = False,
+        timeout: int = 3,
         reset: bool = False,
     ) -> None:
         """
@@ -86,7 +90,7 @@ class Discover:
             will be used for the whois. Use with care.
         """
         if reset:
-            self.discoveredDevices = set()
+            self.discoveredDevices = {}
         found = []
         _networks = []
 
@@ -118,47 +122,69 @@ class Discover:
                 if isinstance(networks, int) and networks < 65535:
                     _networks.append(networks)
 
-        if _networks:
-            for network in _networks:
-                self._log.info(f"Discovering network {network}")
+        if _networks and not global_broadcast:
+            for each_network in _networks:
+                self._log.info(f"Discovering network {each_network}")
                 _res = await self.this_application.app.who_is(
                     low_limit=deviceInstanceRangeLowLimit,
                     high_limit=deviceInstanceRangeHighLimit,
-                    # global_broadcast=global_broadcast, if not address -> global
+                    address=Address(f"{each_network}:*"),
+                    timeout=timeout,
                 )
                 for each in _res:
-                    found.append((each,network))
+                    found.append((each, each_network))
 
         else:
-            self._log.info(
-                "No BACnet network found, attempting a simple whois using provided device instances limits ({} - {})".format(
-                    deviceInstanceRangeLowLimit, deviceInstanceRangeHighLimit
-                )
+            msg = (
+                "Global braodacast required"
+                if global_broadcast
+                else "No BACnet network found"
             )
+            self._log.info(
+                f"{msg}, attempting a simple whois using provided device instances limits ({deviceInstanceRangeLowLimit} - {deviceInstanceRangeHighLimit})"
+            )
+            if global_broadcast is True:
+                self._log.warning(
+                    "Issuing a global Broadcast can create network flood. Use with care."
+                )
+            else:
+                self._log.info("Issuing a local broadcast whois request.")
+            _address = None if global_broadcast is True else LocalBroadcast()
             _res = await _app.who_is(
                 low_limit=deviceInstanceRangeLowLimit,
                 high_limit=deviceInstanceRangeHighLimit,
-                # global_broadcast=global_broadcast,
+                address=_address,
+                timeout=timeout,
             )
             for each in _res:
                 found.append((each, _this_network))
 
-
-        for dev, network_number in found:
+        for iam_request, network_number in found:
+            self._log.debug(f"Found device {iam_request} on network {network_number}")
             if not self.discoveredDevices:
-                self.discoveredDevices = set()  # we can add device as we found some...
-            device_address: Address = dev.pduSource
-            objid: ObjectIdentifier = dev.iAmDeviceIdentifier
-            self.discoveredDevices.add((objid, device_address, network_number))
+                self.discoveredDevices = {}  # we can add device as we found some...
+            device_address: Address = iam_request.pduSource
+            objid: ObjectIdentifier = iam_request.iAmDeviceIdentifier
+            self._log.debug(str(objid))
+            self._log.debug(self.discoveredDevices.keys())
+            key = str(objid)
+            if key in self.discoveredDevices:
+                self._log.debug(
+                    f"{objid} already in discovered devices. Adding network number {network_number} to the list."
+                )
+                self.discoveredDevices[key]["network_number"].add(network_number)
+            else:
+                self._log.debug(
+                    f"Adding {objid} to discovered devices in network {network_number}."
+                )
+                self.discoveredDevices[key] = {
+                    "object_instance": objid,
+                    "address": device_address,
+                    "network_number": {network_number},
+                    "vendor_id": iam_request.vendorID,
+                    "vendor_name": "unknown",
+                }
 
-        if RICH:
-            console = Console()
-            table = Table(show_header=True, header_style="bold magenta")
-            table.add_column("Device Instance")
-            table.add_column("Network Number")
-            table.add_column("Address")
-            for each in self.discoveredDevices:
-                objid, device_address, network_number = each
-                table.add_row(f"{objid}",f"{network_number}",f"{device_address}")
-            console.print(table)
-        return found
+        self._log.info(
+            f"Discovery done. Found {len(self.discoveredDevices)} devices on {len(_networks)} BACnet networks."
+        )
