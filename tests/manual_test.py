aea0e004b412f59069987d561c456ac6161459e7
Christian Tremblay, ing. <christian.tremblay@servisys.com>
2024-09-10 00:09:21 -0400

    ajout du manual test et un script pour tester avant pytest...

---
+++ tests/manual_test.py
@@ -0,0 +1,21 @@
+import asyncio
+import BAC0
+from BAC0.scripts.script_runner import run
+
+bacnet = None
+
+async def main():
+    # Configuration    
+    # # Initialize BACnet
+    print(BAC0.infos.__version__)
+    
+    async with BAC0.start(ip="127.0.0.1/24") as bacnet:
+
+        await bacnet._discover()
+        lst = await bacnet._devices(_return_list=True)
+        print(lst)
+
+
+if __name__ == "__main__":
+    #run(main, bacnet) # Run the script and deals with SIGINT and SIGTERM, useful for long time runnign scripts.
+    asyncio.run(main())
