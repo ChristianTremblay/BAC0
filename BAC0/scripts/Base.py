27e93394f5b1a9742d4e3ae20f68a6ed3f9e5fe3
rvn02 <87974083+rvn02@users.noreply.github.com>
2025-08-21 13:27:49 +0200

    Update Base.py add self argument

--- BAC0/scripts/Base.py
+++ BAC0/scripts/Base.py
@@ -331,7 +331,7 @@ class Base:
         return self._routers
 
     @classmethod
-    def extract_value_from_primitive_data(value):
+    def extract_value_from_primitive_data(self, value):
         if isinstance(value, float):
             return float(value)
         # elif isinstance(value, Boolean):
