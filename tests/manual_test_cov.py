47c7ba1179f7415287218ec42009d039ca83bdd4
Christian Tremblay, ing. <christian.tremblay@servisys.com>
2025-08-24 21:48:24 -0400

    Testing COV, fixing test to get rid of https://github.com/JoelBender/BACpypes3/issues/73 by using async with for all instances
    Manual test for influxdb3. Won't work for easily as it need a InfluxDB3 server, token, etc...

--- tests/manual_test_cov.py
+++ tests/manual_test_cov.py
@@ -12,28 +12,32 @@ bacnet = None
 
 async def main():
     async with BAC0.start() as bacnet:
-        device = BAC0.start(port=47809, deviceId=123)
+        async with BAC0.start(port=47809, deviceId=123) as device:
 
-        new_obj = analog_value(presentValue=0)
-        new_obj.add_objects_to_application(device)
+            new_obj = analog_value(presentValue=0)
+            new_obj.add_objects_to_application(device)
 
-        # From Server
-        dev_av = device.this_application.app.get_object_name("AV")
-        print(dev_av.covIncrement)
+            # From Server
+            dev_av = device.this_application.app.get_object_name("AV")
+            print(dev_av.covIncrement)
 
-        # From client
-        ip = device.localIPAddr.addrTuple[0]
-        boid = device.Boid
-        bacnet._log.info("Defining device with poll 0 so the AV won't get updated")
-        dev = await BAC0.device(f"{ip}:47809", boid, bacnet, poll=0)
-        av = dev["AV"]
-        bacnet._log.info("Subscribing to AV")
-        await dev["AV"].subscribe_cov(lifetime=90)
+            # From client
+            ip = device.localIPAddr.addrTuple[0]
+            boid = device.Boid
+            bacnet._log.info("Defining device with poll 0 so the AV won't get updated")
+            dev = await BAC0.device(f"{ip}:47809", boid, bacnet, poll=0)
+            bacnet._log.info("Subscribing to AV")
 
-        while True:
-            dev_av.presentValue = Real(random.randint(1, 100))
-            bacnet._log.info(f"Setting AV to {dev_av.presentValue}")
-            await asyncio.sleep(5)
+
+            def my_point_callback(property_identifier, property_value):
+                print(f"CALLBACK {property_identifier}: {property_value}")
+
+            await dev["AV"].subscribe_cov(lifetime=90, callback=my_point_callback)
+
+            while True:
+                dev_av.presentValue = Real(random.randint(1, 100))
+                bacnet._log.info(f"Setting AV to {dev_av.presentValue}")
+                await asyncio.sleep(5)
 
 
 if __name__ == "__main__":
